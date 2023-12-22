a2684dd49615c0d14f2b06b32626d983bac0189e
AroiJ <aroij@live.com>
2023-12-23 00:36:04 +0700

    jubjang

--- ex-4.js
+++ ex-4.js
@@ -1,6 +1,9 @@
 // Exercise #4: Displaying Restaurant Name
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
-// Start coding here
-
+let i = 0
+while (i < restaurants.length){
+    console.log("Restaurant Name: " + restaurants[i])
+    i++
+}
 console.log(newRestaurants);
